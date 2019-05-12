83b053dbd90103715ca2fd92ac3b549da663a562
Larry.Eliemenye <denachural@gmail.com>
2019-05-12 12:24:03 +0100

    Install docker elastic agent into plugins directory

--- scripts/setup-gocd.sh
+++ scripts/setup-gocd.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+ELASTIC_AGENT_VERSION="4.0.0"
+ELASTIC_AGENT_BUILD_NUMBER="${ELASTIC_AGENT_VERSION}-126"
+ELASTIC_AGENT_FILENAME="docker-swarm-elastic-agents-${ELASTIC_AGENT_BUILD_NUMBER}.jar"
+
 echo "Preparing Repository..."
 echo "deb https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
 curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo apt-key add -
@@ -13,3 +17,7 @@ sudo apt-get install -y openjdk-8-jre
 echo "Installing and starting go server"
 sudo apt-get install go-server
 sudo /etc/init.d/go-server start
+
+echo "Installing Elastic Agent plugin"
+wget "https://github.com/gocd-contrib/docker-swarm-elastic-agents/releases/download/${ELASTIC_AGENT_VERSION}/${ELASTIC_AGENT_FILENAME}"
+sudo mv $ELASTIC_AGENT_FILENAME "/var/lib/go-server/plugins/external/${ELASTIC_AGENT_FILENAME}"
