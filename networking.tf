91101d485ccb99936aed2c7fe6b0e10a23d65ddc
Larry.Eliemenye <denachural@gmail.com>
2019-07-10 03:23:03 +0100

    remove useless ftp access in security group

--- networking.tf
+++ networking.tf
@@ -70,24 +70,6 @@ resource "aws_security_group" "allow_gocd_http_port" {
   }
 }
 
-resource "aws_security_group" "allow_ftp" {
-  name = "allow_ftp"
-
-  ingress {
-    from_port = 23
-    protocol = "tcp"
-    to_port = 23
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_eip" "gocd_eip" {
   instance = "${aws_instance.gocd_instance.id}"
 }
